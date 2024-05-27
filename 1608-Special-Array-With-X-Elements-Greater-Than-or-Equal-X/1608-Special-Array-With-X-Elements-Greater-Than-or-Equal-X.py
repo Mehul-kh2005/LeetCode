774fd62203fcbdba5331bef2216be28c65b97e99
Mehul Khandelwal <mehul.kh2005@gmail.com>
2024-05-27 09:39:39 +0530

    File 1608

---
+++ 1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X/1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X.py
@@ -0,0 +1,11 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        bucket=[0]*1001
+        for num in nums:
+            bucket[num]+=1  
+        total=len(nums)        
+        for i in range(1001):
+            if i==total:
+                return i
+            total-=bucket[i]       
+        return -1
