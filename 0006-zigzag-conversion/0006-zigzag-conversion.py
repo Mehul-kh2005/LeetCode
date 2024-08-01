90a44193b723c4c6185d299839f2657cd6b72eed
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-08-01 16:42:50 +0530

    Time: 50 ms (64.91%), Space: 16.7 MB (49.82%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,22 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        # Edge case: If numRows is 1 or greater than or equal to the length of s, return s
-        if numRows == 1 or numRows >= len(s):
+        if(numRows  < 2):
             return s
-
-        # Create a list of empty strings for each row
-        rows = ['' for _ in range(numRows)]
-        current_row = 0
-        going_down = False
-
-        # Build the zigzag pattern
+        arr = ['' for i in range(numRows)]
+        direction = 'down'
+        row = 0
         for char in s:
-            rows[current_row] += char
-            # Change direction if we are at the top or bottom row
-            if current_row == 0 or current_row == numRows - 1:
-                going_down = not going_down
-            # Move to the next row
-            current_row += 1 if going_down else -1
-
-        # Join all rows to get the final string
-        return ''.join(rows)
+            arr[row] += char
+            if row == numRows-1:
+                direction = 'up'
+            elif row == 0:
+                direction = 'down'
+            if(direction == 'down'):
+                row += 1
+            else:
+                row -= 1
+        return(''.join(arr))
