8740711986b9356b2f516be9289bc17057b8c679
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-08-01 10:10:25 +0530

    Time: 141 ms (87.66%), Space: 17.9 MB (9.52%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -5,8 +5,8 @@ class Solution:
 
         for i in range(1,n):
             if nums[i]<nums[i-1]:
+                if count or (i>1 and i<n-1 and nums[i]<nums[i-2] and nums[i+1]<nums[i-1]):
+                    return False
                 count+=1
         
-        if count==1:
-            return True
-        return False
+        return True
