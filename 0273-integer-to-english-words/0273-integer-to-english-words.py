240f7d674b5eb4317eceda819a28b612378de884
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-08-07 17:43:14 +0530

    Time: 40 ms (33.87%), Space: 16.6 MB (47.47%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -1,32 +1,30 @@
 class Solution:
-    # Dictionary to store words for numbers
-    number_to_words_map = {
-        1000000000: "Billion", 1000000: "Million", 1000: "Thousand",
-        100: "Hundred", 90: "Ninety", 80: "Eighty", 70: "Seventy",
-        60: "Sixty", 50: "Fifty", 40: "Forty", 30: "Thirty",
-        20: "Twenty", 19: "Nineteen", 18: "Eighteen", 17: "Seventeen",
-        16: "Sixteen", 15: "Fifteen", 14: "Fourteen", 13: "Thirteen",
-        12: "Twelve", 11: "Eleven", 10: "Ten", 9: "Nine", 8: "Eight",
-        7: "Seven", 6: "Six", 5: "Five", 4: "Four", 3: "Three",
-        2: "Two", 1: "One"
-    }
-
     def numberToWords(self, num: int) -> str:
+        def helper(num):
+        
+            if num < 20:
+                s = belowTwenty[num]
+            elif num < 100:
+                s = tens[num // 10] + ' ' + belowTwenty[num % 10]
+            elif num < 1000:
+                s = helper(num // 100) + ' Hundred ' + helper(num % 100)
+            elif num < 1000000:
+                s = helper(num // 1000) + ' Thousand ' + helper(num % 1000)
+            elif num < 1000000000:
+                s = helper(num // 1000000) + ' Million ' + helper(num % 1000000)
+            else:
+                s = helper(num // 1000000000) + ' Billion ' + helper(num % 1000000000)
+
+            return s.strip()
+
         if num == 0:
-            return "Zero"
+            return 'Zero'
 
-        for value, word in self.number_to_words_map.items():
-            # Check if the number is greater than or equal to the current unit
-            if num >= value:
-                # Convert the quotient to words if the current unit is 100 or greater
-                prefix = (self.numberToWords(num // value) + " ") if num >= 100 else ""
+        belowTwenty = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight',
+                       'Nine', 'Ten', 'Eleven', 'Twelve','Thirteen', 'Fourteen', 'Fifteen',
+                       'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
 
-                # Get the word for the current unit
-                unit = word
+        tens = ['', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 
+                'Seventy', 'Eighty', 'Ninety']
 
-                # Convert the remainder to words if it's not zero
-                suffix = "" if num % value == 0 else " " + self.numberToWords(num % value)
-
-                return prefix + unit + suffix
-
-        return ""
+        return helper(num)
