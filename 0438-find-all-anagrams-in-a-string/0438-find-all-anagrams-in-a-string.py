142670a28feecf7903180cbdb3544316d3ac660a
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-08-08 18:51:38 +0530

    Time: 282 ms (23.15%), Space: 17.7 MB (50.19%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,16 +1,34 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        len_p,len_s=len(p),len(s)
+        len_p, len_s = len(p), len(s)
 
-        if len_p>len_s:
+        if len_p > len_s:
             return []
 
-        p="".join(sorted(p))
-        result=[]
+        # Count frequency of characters in p
+        p_count = Counter(p)
+        
+        # Count frequency of characters in the initial window of s
+        s_count = Counter(s[:len_p])
+        
+        result = []
 
-        for i in range(0,len_s-len_p+1):
-            word="".join(sorted(s[i:i+len_p]))
-            if word==p:
-                result.append(i) 
+        # Check if initial window is an anagram
+        if s_count == p_count:
+            result.append(0)
+        
+        # Slide the window across s
+        for i in range(1, len_s - len_p + 1):
+            # Remove the character going out of the window
+            s_count[s[i - 1]] -= 1
+            if s_count[s[i - 1]] == 0:
+                del s_count[s[i - 1]]
+            
+            # Add the new character coming into the window
+            s_count[s[i + len_p - 1]] += 1
+            
+            # Compare frequency counts
+            if s_count == p_count:
+                result.append(i)
         
         return result
