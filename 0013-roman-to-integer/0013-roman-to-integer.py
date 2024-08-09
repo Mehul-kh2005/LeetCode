3b9fe6c647b7c15e2e27befc0eedf64501dec7a5
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-08-09 18:26:48 +0530

    Time: 44 ms (71.04%), Space: 16.7 MB (9.14%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,36 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_values = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-
-        resp = 0
-        index = 0
-        for element in s:
-
-            current = roman_values[element]
-
-            if index + 1 == len(s):
-                resp = resp + current
-
-            elif element == 'C' and (s[index+1] == 'M' or  s[index+1] == 'D'):
-                resp = resp - 100
-
-            elif element == 'X' and (s[index+1] == 'L' or  s[index+1] == 'C'):
-                resp = resp - 10
-
-            elif element == 'I' and (s[index+1] == 'V' or  s[index+1] == 'X'):
-                resp = resp - 1
-
-            else:
-                resp = resp + current
-
-            index += 1
-
-        return resp
+        
+        # create a hashmap for our values
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        # create a result value to be returned
+        result = 0
+        
+        for i in range(len(s)):
+            # check if there is a "next value" (i + 1)
+            # if there is then check if the next value is greater than the previous
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                # if the previous value is less, we subtract that value from the result
+                result -= roman[s[i]]
+            # otherwise we add it to our result
+            else: 
+                result += roman[s[i]]
+            # we return the result we find
+        return result
