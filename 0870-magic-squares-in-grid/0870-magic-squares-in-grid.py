f60cf6f4b735037500d002d1b0af9f2dc2d5820e
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-08-09 18:23:13 +0530

    Time: 46 ms (21.03%), Space: 16.7 MB (5.13%) - LeetHub

--- 0870-magic-squares-in-grid/0870-magic-squares-in-grid.py
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.py
@@ -4,17 +4,17 @@ class Solution:
             numbers = matrix[0] + matrix[1] + matrix[2]
             if sorted(numbers) != list(range(1, 10)):
                 return False
-                
-            return (len(matrix)==3 and all(len(row)==3 for row in matrix) and 
-            len(set(matrix[0]+matrix[1]+matrix[2]))==9 and 
-            sum(matrix[0])==15 and 
-            sum(matrix[1])==15 and
-            sum(matrix[2])==15 and
-            sum(matrix[i][0] for i in range(3))==15 and 
-            sum(matrix[i][1] for i in range(3))==15 and
-            sum(matrix[i][2] for i in range(3))==15 and
-            sum(matrix[i][i] for i in range(3))==15 and
-            sum(matrix[i][2-i] for i in range(3))==15)
+
+            return (
+                sum(matrix[0]) == 15 and
+                sum(matrix[1]) == 15 and
+                sum(matrix[2]) == 15 and
+                sum(matrix[i][0] for i in range(3)) == 15 and
+                sum(matrix[i][1] for i in range(3)) == 15 and
+                sum(matrix[i][2] for i in range(3)) == 15 and
+                sum(matrix[i][i] for i in range(3)) == 15 and
+                sum(matrix[i][2 - i] for i in range(3)) == 15
+            )
 
         rows,cols=len(grid),len(grid[0])
         if rows<3 or cols<3:
