1ae1b9a63bcafddf0ed09f0c2e1711d946ec00d6
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-08-21 18:21:09 +0530

    Time: 92 ms (96.34%), Space: 16.7 MB (54.95%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -1,39 +1,25 @@
 class Solution:
     def strangePrinter(self, s: str) -> int:
-        # Preprocess the string to remove consecutive duplicate characters
-        s = "".join(char for char, _ in itertools.groupby(s))
         n = len(s)
-
-        # min_turns[i][j] represents the minimum number of turns to print s[i] to s[j]
-        min_turns = [[0] * n for _ in range(n)]
-
-        # Initialize base case
+        a = [s[0]]
+        for i in range(1, n):
+            if s[i] != s[i - 1]:
+                a.append(s[i])
+        n = len(a)
+        h = {}
+        t = [n] * n
+        for i in reversed(range(n)):
+            if a[i] in h:
+                t[i] = h[a[i]]
+            h[a[i]] = i
+        d = [[0] * n for _ in range(n + 1)]
         for i in range(n):
-            # It takes 1 turn to print a single character
-            min_turns[i][i] = 1
-
-        # Fill the dp table
-        for length in range(2, n + 1):
-            for start in range(n - length + 1):
-                end = start + length - 1
-
-                # Initialize with worst case: print each character separately
-                min_turns[start][end] = length
-
-                # Try all possible splits and find the minimum
-                for split in range(length - 1):
-                    total_turns = (
-                        min_turns[start][start + split]
-                        + min_turns[start + split + 1][end]
-                    )
-
-                    # If the characters at the split and end match, we can save one turn
-                    if s[start + split] == s[end]:
-                        total_turns -= 1
-
-                    min_turns[start][end] = min(
-                        min_turns[start][end], total_turns
-                    )
-
-        # Return the minimum turns needed to print the entire string
-        return min_turns[0][n - 1] if n > 0 else 0
+            d[i][i] = 1
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                d[i][j] = 1 + d[i + 1][j]
+                k = t[i]
+                while k <= j:
+                    d[i][j] = min(d[i][j], d[i][k - 1] + d[k + 1][j])
+                    k = t[k]
+        return d[0][n - 1]
