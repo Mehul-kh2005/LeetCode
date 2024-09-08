18fa6d2aa460eb44f6deda8344a75006069feb32
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-09-08 20:34:19 +0530

    Time: 52 ms (14.29%), Space: 17.1 MB (20.88%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        flattened=sorted([item for sublist in matrix for item in sublist])
+
+        index=bisect.bisect_left(flattened,target)
+
+        if index<len(flattened) and flattened[index]==target:
+            return True
+        else:
+            return False
