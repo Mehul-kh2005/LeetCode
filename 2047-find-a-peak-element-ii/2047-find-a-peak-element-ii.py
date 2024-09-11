802868026d195c1c59b0bdf0aa5541f1d30c8d16
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-09-11 18:20:34 +0530

    Time: 893 ms (70.8%), Space: 48 MB (12.9%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.py
@@ -5,17 +5,15 @@ class Solution:
 
         while left < right:
             mid = left + (right - left) // 2
-            # Find the index of the maximum element in the middle row
+          
             max_idx_mid = mat[mid].index(max(mat[mid]))
-            # Find the index of the maximum element in the next row
+           
             max_idx_next = mat[mid + 1].index(max(mat[mid + 1]))
 
-            # Compare the peak values in the current row and the next row
             if mat[mid][max_idx_mid] < mat[mid + 1][max_idx_next]:
                 left = mid + 1
             else:
                 right = mid
 
-        # Find the index of the peak element in the final row
         peak_idx = mat[left].index(max(mat[left]))
         return [left, peak_idx]
