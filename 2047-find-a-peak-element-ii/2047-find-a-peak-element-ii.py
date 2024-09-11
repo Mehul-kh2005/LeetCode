a2416f65270274bbb2b493a3f4790aba2255c4af
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-09-11 18:19:38 +0530

    Time: 891 ms (75.3%), Space: 47.8 MB (80.9%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.py
@@ -1,21 +1,10 @@
 class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
-        left = 0
-        right = len(mat) - 1
+        dict1={}
 
-        while left < right:
-            mid = left + (right - left) // 2
-            # Find the index of the maximum element in the middle row
-            max_idx_mid = mat[mid].index(max(mat[mid]))
-            # Find the index of the maximum element in the next row
-            max_idx_next = mat[mid + 1].index(max(mat[mid + 1]))
+        for i in range(len(mat)):
+            ele=max(mat[i])
+            idx=mat[i].index(ele)
+            dict1[ele]=(i,idx)
 
-            # Compare the peak values in the current row and the next row
-            if mat[mid][max_idx_mid] < mat[mid + 1][max_idx_next]:
-                left = mid + 1
-            else:
-                right = mid
-
-        # Find the index of the peak element in the final row
-        peak_idx = mat[left].index(max(mat[left]))
-        return [left, peak_idx]
+        return dict1[max(dict1)]
