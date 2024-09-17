e0d9874f0a45b01b43eb9e4f3aaee8f7a0a416b1
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-09-17 20:49:52 +0530

    Time: 31 ms (86.55%), Space: 16.6 MB (32.61%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,25 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n=len(nums)
+        n = len(nums)
 
-        if n==1:
+        if n == 1:
             return nums[0]
-        if n==2:
-            return max(nums[0],nums[1])
 
-        dp1=[0]*(n-1)
-        dp2=[0]*(n-1)
+        def rob_linear(houses):
+            dp = [0] * len(houses)
+            dp[0] = houses[0]
+            if len(houses) > 1:
+                dp[1] = max(houses[0], houses[1])
 
-        dp1[0]=nums[0]
-        dp1[1]=max(nums[0],nums[1])
+            for i in range(2, len(houses)):
+                dp[i] = max(dp[i-1], dp[i-2] + houses[i])
 
-        dp2[0]=nums[1]
-        dp2[1]=max(nums[1],nums[2])
+            return dp[-1]
 
-        for i in range(2,n-1):
-            dp1[i]=max(dp1[i-1],dp1[i-2]+nums[i])
+        rob_first = rob_linear(nums[:-1])
+        rob_last = rob_linear(nums[1:])
 
-        for i in range(3,n):
-            dp2[i-1]=max(dp2[i-2],dp2[i-3]+nums[i])
-
-        return max(dp1[-1],dp2[-1])
+        return max(rob_first, rob_last)
