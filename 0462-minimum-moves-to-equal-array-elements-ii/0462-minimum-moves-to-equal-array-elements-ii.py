fc0be9c69d511b3473c85e15df4181eff130d9ec
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-10-04 11:42:14 +0530

    Time: 89 ms (6.51%), Space: 18 MB (68.25%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.py
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.py
@@ -1,9 +1,5 @@
-from bisect import insort
-
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
-        arr=[]
-        for num in nums:
-            insort(arr,num)
-        median=arr[len(arr)//2]
-        return sum(abs(median-num) for num in arr)
+        nums.sort()
+        median=nums[len(nums)//2]
+        return sum(abs(median-num) for num in nums)
