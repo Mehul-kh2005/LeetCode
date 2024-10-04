a30583219ae57f0905d34a1150342491dea2ea0f
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-10-04 11:20:38 +0530

    Time: 1533 ms (5.13%), Space: 29.6 MB (31.15%) - LeetHub

---
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,17 @@
+import bisect
+
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        sorted_arr=[]
+        total_chemistry=0
+        for s in skill:
+            bisect.insort(sorted_arr,s)
+
+        target_sum=sorted_arr[0]+sorted_arr[-1]
+        
+        for i in range(len(sorted_arr)//2):
+            if sorted_arr[i]+sorted_arr[-i-1]!=target_sum:
+                return -1
+            total_chemistry+=sorted_arr[i]*sorted_arr[-i-1]
+
+        return total_chemistry
