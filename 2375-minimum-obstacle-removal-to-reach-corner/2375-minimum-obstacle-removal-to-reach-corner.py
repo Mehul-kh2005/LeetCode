f16c2c6e9ab89eb8e2b71979c5939378a1995072
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2024-11-28 13:09:56 +0530

    Time: 1581 ms (49.8%), Space: 42.8 MB (65.1%) - LeetHub

---
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.py
@@ -0,0 +1,29 @@
+class Solution:       
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        def isValid(row,col):
+            return 0<=row<len(grid) and 0<=col<len(grid[0])
+
+        directions=[(0,1),(0,-1),(1,0),(-1,0)]
+        m=len(grid)
+        n=len(grid[0])
+
+        minimumObstacles=[[float('inf')]*n for _ in range(m)]
+        minimumObstacles[0][0]=0
+        dq=deque([(0,0,0)])
+
+        while dq:
+            obstacle,row,col=dq.popleft()
+
+            for dr,dc in directions:
+                new_row,new_col=row+dr,col+dc
+
+                if isValid(new_row,new_col) and minimumObstacles[new_row][new_col]==float('inf'):
+                    if grid[new_row][new_col]==1:
+                        minimumObstacles[new_row][new_col]=obstacle+1
+                        dq.append((obstacle+1,new_row,new_col))
+                        
+                    else:
+                        minimumObstacles[new_row][new_col]=obstacle
+                        dq.appendleft((obstacle,new_row,new_col))
+
+        return minimumObstacles[m-1][n-1]
