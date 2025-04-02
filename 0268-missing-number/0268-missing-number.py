b0777b9e2ce9d2b4e398bc13a86f74abc534a3dd
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2025-04-02 16:57:18 +0530

    Time: 0 ms (100%), Space: 18.8 MB (52.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n=len(nums)
-        seen=[False]*(n+1)
-
-        for num in nums:
-            if 0<=num<=n:
-                seen[num]=True
-
-        for i in range(n+1):
-            if not seen[i]:
-                return i
-
-        return i+1
+        total_sum=n*(n+1)//2
+        curr_sum=sum(nums)
+        return total_sum-curr_sum
