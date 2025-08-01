c7a1fe6b53fc0cd760d8596a801c51dcb8b7366a
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2025-08-01 10:23:46 +0530

    Time: 0 ms (100%), Space: 19.3 MB (20.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,27 +1,43 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def binarySearch(nums,target,findFirst):
-            low,high=0,len(nums)-1
-            result=-1
+        def findFirst(nums):
+            left=0
+            right=len(nums)-1
+            index=-1
 
-            while low<=high:
-                mid=(low+high)//2
+            while left<=right:
+                mid=(left+right)//2
 
                 if nums[mid]==target:
-                    result=mid
-                    if findFirst:
-                        high=mid-1
-                    else:
-                        low=mid+1
-                
+                    index=mid
+                    right=mid-1
+
                 elif nums[mid]<target:
-                    low=mid+1
+                    left=mid+1
 
                 else:
-                    high=mid-1
+                    right=mid-1
 
-            return result
+            return index
 
-        first=binarySearch(nums,target,True)
-        last=binarySearch(nums,target,False)
-        return [first,last]
+        def findLast(nums):
+            left=0
+            right=len(nums)-1
+            index=-1
+
+            while left<=right:
+                mid=(left+right)//2
+
+                if nums[mid]==target:
+                    index=mid
+                    left=mid+1
+
+                elif nums[mid]<target:
+                    left=mid+1
+
+                else:
+                    right=mid-1
+
+            return index
+
+        return [findFirst(nums),findLast(nums)]
