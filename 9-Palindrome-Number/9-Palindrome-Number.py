cde9ffaa35baf192172ca7a96c7454699ac9dc3d
Mehul Khandelwal <mehul.kh2005@gmail.com>
2025-09-14 12:28:45 +0530

    Files Changed

--- 9-Palindrome-Number/9-Palindrome-Number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        x1=str(x)
-        l=[]
-        for i in range(len(x1)):
-            l.append(x1[i])
-        l.reverse()
-        x2=''.join(l)
-        return x1==x2
